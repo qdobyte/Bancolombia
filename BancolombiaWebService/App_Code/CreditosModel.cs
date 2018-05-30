59defe1d0128b55e62d76c096e00a570f33799c9
QdoByte <qdobyte@gmail.com>
2018-05-30 17:13:14 -0500

    Primer commit

---
+++ BancolombiaWebService/App_Code/CreditosModel.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Descripción breve de CreditosModel
+/// </summary>
+public class CreditosModel
+{
+    public int IdCredito;
+    public string Nombre;
+    public int Monto;
+    public int Plazo;
+    public float Tea;
+    public float Tnm;
+    public int TotalInteres;
+    public int TotalMontoInteres;
+
+    public string Error;
+
+    private ConexionSQL conexionSQL = null;
+    private SqlCommand sqlCommand;
+    
+    public CreditosModel()
+    {
+        this.conexionSQL = new ConexionSQL();
+        this.sqlCommand = new SqlCommand();
+    }
+
+    public bool InsertarCredito(string producto, int plazo, int monto, int idCliente)
+    {
+
+        float tea = this.GetTEA(producto);
+        float tnm = this.GetTNM(tea);
+        int anualidad = this.CalculateAnualidad(monto, plazo, tnm);
+        int totalInteres = this.CalculateTotalInteres(anualidad, plazo, monto);
+        int totalMontoIntereses = monto + totalInteres;
+
+        sqlCommand.CommandType = CommandType.StoredProcedure;
+        sqlCommand.CommandText = "crearCredito";
+        sqlCommand.Parameters.AddWithValue("@Plazo", plazo); 
+        sqlCommand.Parameters.AddWithValue("@Monto", monto); 
+        sqlCommand.Parameters.AddWithValue("@TotalInteres", totalInteres); 
+        sqlCommand.Parameters.AddWithValue("@Anualidad", anualidad); 
+        sqlCommand.Parameters.AddWithValue("@Tnm", tnm); 
+        sqlCommand.Parameters.AddWithValue("@Tea", tea); 
+        sqlCommand.Parameters.AddWithValue("@IdCliente", idCliente); 
+        sqlCommand.Parameters.AddWithValue("@TotalMontoIntereses", totalMontoIntereses); 
+
+        bool consultaExitosa = conexionSQL.ExecuteStoreProcedure(sqlCommand);
+        sqlCommand = null;
+
+        if (consultaExitosa)
+        {
+            return true;
+        }
+        else
+        {
+            this.Error = conexionSQL.ErrorMessage;
+            return false;
+        }
+    }
+
+    public List<CreditosModel> ConsultarProductosPorCliente(string cedula)
+    {
+        sqlCommand.CommandType = CommandType.StoredProcedure;
+        sqlCommand.CommandText = "consultarProductosPorCliente";
+        sqlCommand.Parameters.AddWithValue("@Cedula", cedula);
+
+        bool consultaExitosa = conexionSQL.ExecuteStoreProcedure(sqlCommand);
+        sqlCommand = null;
+        List<CreditosModel> lista = new List<CreditosModel> { };
+        if (consultaExitosa)
+        {
+            if (conexionSQL.data.Rows.Count > 0)
+            {
+                foreach (DataRow row in conexionSQL.data.Rows)
+                {
+                    lista.Add(new CreditosModel()
+                    {
+                        IdCredito = Int32.Parse(row["id_credito"].ToString()),
+                        Nombre = row["nombre"].ToString(),
+                        Monto = Int32.Parse(row["monto"].ToString()),
+                        Plazo = Int32.Parse(row["plazo"].ToString()),
+                        Tea = float.Parse(row["tea"].ToString()),
+                        Tnm = float.Parse(row["tnm"].ToString()),
+                        TotalInteres = Int32.Parse(row["total_interes"].ToString()),
+                        TotalMontoInteres = Int32.Parse(row["total_monto_intereses"].ToString()),
+                    });
+                }
+            }
+            else
+            {
+                lista.Add(new CreditosModel() { Error = "No se encontraron creditos para el cliente seleccionado" });
+            }
+        }
+        else
+        {
+            lista.Add(new CreditosModel() { Error = conexionSQL.ErrorMessage });
+        }
+        return lista;
+    }
+
+    #region Calculos
+    private float GetTEA(string producto) {
+        float tea;
+        switch (producto) {
+            case "libreInversion":
+                tea = 32;
+                break;
+            case "vehiculo":
+                tea = 24;
+                break;
+            case "vivienda":
+                tea = 12;
+                break;
+            default:
+                tea = 0;
+                break;
+        }
+        return tea;
+    }
+
+    private float GetTNM(float tnm) {
+        switch (tnm)
+        {
+            case 32:
+                tnm = 2.34f;
+                break;
+            case 24:
+                tnm = 1.8f;
+                break;
+            case 12:
+                tnm = 0.94f;
+                break;
+            default:
+                tnm = 0;
+                break;
+        }
+        return tnm;
+    }
+
+    private int CalculateAnualidad(int monto, int plazo, float tnm) {
+        float exponencial = (float) Math.Pow( (tnm / 100) + 1 , plazo);
+        return Convert.ToInt32(  monto * (exponencial * (tnm/100)) / (exponencial - 1) );
+    }
+
+    private int CalculateTotalInteres(int anualidad, int plazo, int monto) {
+        return ( anualidad * plazo ) - monto;
+    }
+    #endregion
+}
---
+++ SQLServer/CRUD.sql
@@ -0,0 +1,138 @@
+use Bancolombia
+go
+/*
+DROP PROCEDURE crearUsuario; 
+GO
+*/
+create proc insertarCliente
+	@Cedula varchar(50),
+	@Nombre varchar(50),
+	@Direccion varchar(50),
+	@Ingresos int,
+	@Egresos int,
+	@Pasivos int,
+	@Scoring int
+
+As 
+	INSERT INTO Cliente (cedula,nombre,direccion,ingresos,egresos,pasivos,scoring)
+	VALUES (@Cedula,@Nombre,@Direccion,@Ingresos,@Egresos,@Pasivos,@Scoring); 
+	
+go
+
+exec crearUsuario '345','ccc','cl 47 b',1000,500,100,250
+
+use Bancolombia
+go
+/*
+DROP PROCEDURE modificarUsuario; 
+GO
+*/
+create proc modificarCliente
+	@IdCliente int,
+	@Cedula varchar(50),
+	@Nombre varchar(50),
+	@Direccion varchar(50),
+	@Ingresos int,
+	@Egresos int,
+	@Pasivos int,
+	@Scoring int
+
+As 
+	UPDATE Cliente
+	SET cedula = @Cedula,nombre = @Nombre,direccion = @Direccion,ingresos = @Ingresos,egresos= @Egresos,pasivos = @Pasivos,scoring = @Scoring
+	WHERE id_cliente = @IdCliente
+	
+go
+
+exec modificarUsuario 1,'234','bbb','cl 45 c',1000,500,100,250
+
+use Bancolombia
+go
+
+/*
+DROP PROCEDURE eliminarUsuario; 
+GO
+*/
+create proc eliminarCliente
+	@IdCliente int
+As
+DELETE FROM Cliente
+WHERE id_cliente = @IdCliente
+go
+
+exec eliminarCliente 1
+
+use Bancolombia
+go
+/*
+DROP PROCEDURE consultarUsuario;  
+GO
+*/  
+create proc consultarCliente
+
+	@Cedula varchar(50)
+	
+As
+SELECT id_cliente,cedula,nombre,direccion,ingresos,egresos,pasivos,scoring FROM Cliente
+WHERE cedula = @Cedula
+go
+
+exec consultarCliente '123'
+
+
+/*LISTAR TODOS LOS USUARIOS*/
+
+/*
+DROP PROCEDURE listarUsuarios;  
+GO
+*/  
+create proc listarClientes
+As
+SELECT id_cliente,cedula,nombre,direccion,ingresos,egresos,pasivos,scoring FROM Cliente
+go
+
+exec listarClientes
+
+use Bancolombia
+go
+/*
+DROP PROCEDURE crearCredito;  
+GO
+*/  
+create proc crearCredito
+	@Plazo int,
+	@Monto int,
+	@TotalInteres int,
+	@Anualidad int,
+	@Tnm float,
+	@Tea float,
+	@IdCliente int,
+	@TotalMontoIntereses int
+
+As 
+	INSERT INTO Credito(plazo,monto,total_interes,anualidad,tnm,tea,id_cliente, total_monto_intereses)
+	VALUES (@Plazo,@Monto,@TotalInteres,@Anualidad,@Tnm,@Tea,@IdCliente, @TotalMontoIntereses); 
+	
+go
+
+exec crearCredito 12,1500,10,15,20,15,10,2,1
+
+
+DROP PROC consultarProductosPorCliente;
+GO
+create proc consultarProductosPorCliente
+	@Cedula varchar(50)
+as
+	select cre.id_credito, cre.nombre, cre.monto, cre.plazo, cre.tea , cre.tnm, cre.total_interes, cre.total_monto_intereses
+	from Cliente as cli
+		inner join Credito as cre
+			on cli.id_cliente = cre.id_cliente
+	where cli.cedula = @Cedula
+go
+
+exec consultarProductosPorCliente '1087986486'
+
+
+
+select * from Credito
+select * from Cliente
